e9db936092bb6da477a0d58bdb0cd35372276c46
gluay88 <p.wantagaew@gmail.com>
2022-02-07 16:26:29 -0800

    api validations

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should validate name' do
+    cat = Cat.create
+    expect(cat.errors[:name]).to_not be_empty
+  end
+
+  it 'should validate age' do
+    cat = Cat.create
+    expect(cat.errors[:age]).to_not be_empty
+  end
+
+  it 'should validate enjoys' do
+    cat = Cat.create
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+
+  it 'should validate image' do
+    cat = Cat.create
+    expect(cat.errors[:image]).to_not be_empty
+  end
 end
