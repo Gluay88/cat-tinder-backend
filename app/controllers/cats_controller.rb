2eb7af1cca06b9b24c5348ab6b733e3f7ed7f73d
gitgood-ENT <ward.noahd@gmail.com>
2022-02-08 13:32:28 -0800

    syntax fixes

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -16,12 +16,16 @@ class CatsController < ApplicationController
   
     def update
         cat = Cat.find(params[:id])
-        cat.update(cat_params)
+        cat.update(cat_params) 
         # Don't forget to wrap each option for the ternary in () or it doesn't work properly
         cat.valid? ? (render json: cat) : (render json: cat.errors, status: 422)
     end
   
     def destroy
+        cat = Cat.find(params[:id])
+        cat.destroy
+        cats = Cat.all
+        render json: cats
     end
 
 
